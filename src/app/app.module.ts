938345823cf6b9515364677508b8851ef6d0a617
InteractVersionControl <david.zhang@ip.com.au>
2017-09-19 15:58:20 +1000

    Routing WORK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
 
-import { AppComponent } from './app.component';
-
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
+export class AppModule { }
 
-export class AppModule {
-
- }
